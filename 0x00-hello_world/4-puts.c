c97f43d2e8d855f96379457e974738ec91cb3f8d
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-16 13:07:52 -0700

    File 4 and a little complicated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - always comes before
+ *
+ * Return: always comes after main
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
