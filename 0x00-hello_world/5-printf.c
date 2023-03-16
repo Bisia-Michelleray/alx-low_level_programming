6bad9552208167e3b4bfc528ad7777de087e733a
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-16 13:28:04 -0700

    File 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - serves as the starting point for program execution
+ *
+ * Return: ends the execution of a function
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
