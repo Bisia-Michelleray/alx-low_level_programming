44be1df24552c5a82654276c5c0e0a79ab691cd9
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-19 13:36:22 -0700

    File 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,13 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
