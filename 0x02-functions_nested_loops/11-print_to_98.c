4afa0db078778abedf61ccd8652effedb2f72454
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-21 15:07:13 -0700

    File 11 printing of alphabets

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -25,7 +27,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
@@ -33,10 +37,12 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
