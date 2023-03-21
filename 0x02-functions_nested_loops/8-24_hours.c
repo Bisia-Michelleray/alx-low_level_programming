a40258d9ae40282f7e2d6e62ca2b58809f8400bc
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-21 11:12:31 -0700

    File 8 hours system

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every hour of the day
+ *
+ * Return: always 0 (success)
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 24 ; i++)
+	{
+		for (j = 0 ; j < 60 ; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
