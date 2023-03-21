481b9dcdedaa2145136278880151f7b3ec23a909
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-21 12:26:07 -0700

    File 10 adding of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that add two integers
+ *
+ * @a: paramater 1 to be checked
+ * @b: paramater 2 to be checked
+ *
+ * Return: results of the outcome
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
