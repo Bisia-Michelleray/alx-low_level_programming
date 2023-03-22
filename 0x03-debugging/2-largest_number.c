f25ead1d19fdecfedd8b134ff0b2a1994c0c05ca
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-22 10:16:54 -0700

    File 2 largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,15 +17,21 @@ int largest_number(int a, int b, int c)
 	largest = a;
 	}
 	else if (a > c && c > b)
+	{
 		largest = a;
+	}
 	else if (b > a && a > c)
 	{
 	largest = b;
 	}
 	else if (b > c && c > a)
+	{
 		largest = b;
+	}
 	else if (c > a && a > b)
+	{
 		largest = c;
+	}
 	else if (c > b && b > a)
 	{
 	largest = c;
