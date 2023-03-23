05b8fce4799f5b4b2ad8184f1fc6ba485ce40078
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-23 10:50:07 -0700

    File 6 printing of a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - draws a staight line in the terminal
+ * @n: number of times character is printed
+ * Return: always 0 (success)
+ */
+
+void print_line(int n)
+{
+	int m;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (m = 0 ; m <= n ; m++)
+		{
+			putchar(95);
+		}
+		putchar('\n');
+	}
+}
