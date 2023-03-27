e982fca0eb12180e6fd5a103be4c84d0a1fd27e3
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-27 11:01:16 -0700

    File 5 reversing a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,4 +8,17 @@
 
 void rev_string(char *s)
 {
-	int i
+	int len = 0;
+	int index = 0;
+	char tmp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1 ; index >= len / 2 ; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+	}
+}
