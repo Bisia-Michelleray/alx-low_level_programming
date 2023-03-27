3eb7a1189c56219e4fc3be44521a25216cbda753
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-27 07:39:32 -0700

    File 0 reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and is replaced
+ * @n: paramater to be replaced
+ * Return: always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
