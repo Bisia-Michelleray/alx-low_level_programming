036683dabf60a1ca352bdca26bf1ef2ff9240c7d
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-27 14:56:15 -0700

    File 9 copying of string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed
+ * @dest: function one
+ * @src: fuction two
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	if (dest == NULL)
+		return (NULL);
+	{
+		for (i = 0 ; src[i] != '\0' ; i++)
+			dest[i] = src[i];
+	}
+	return (dest);
+}
