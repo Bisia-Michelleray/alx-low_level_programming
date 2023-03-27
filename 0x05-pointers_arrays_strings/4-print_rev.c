8c0002de4ce0943261dfbf4c01e3349eb6af4004
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-27 09:50:25 -0700

    File 4 printing in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,11 @@ void print_rev(char *s)
 	int count = 0;
 
 
-	for (i = 0 ; s[i] ; i++)
+	for (i = 0 ; i[s] ; i++)
 		count++;
 	for (i = count ; i >= 0 ; i--)
 	{
-		_putchar(s[i]);
+		_putchar(i[s]);
 	}
 	_putchar('\n');
 }
