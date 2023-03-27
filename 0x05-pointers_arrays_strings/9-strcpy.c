bc69a96f192cbb1ba615c42b4aeaf1858b440c19
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-27 15:17:26 -0700

    File 9 copying of string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,8 @@
 
 /**
  * *_strcpy - copies the string pointed
- * @dest: function one
- * @src: fuction two
+ * @dest: destination value
+ * @src: source value
  * Return: dest
  */
 
@@ -11,9 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	if (dest == NULL)
-		return (NULL);
 	for (i = 0 ; src[i] != '\0' ; i++)
-		dest[i] = src[i];
+	{
+	dest[i] = src[i];
+	}
+	dest[i++] = '\0';
 	return (dest);
 }
