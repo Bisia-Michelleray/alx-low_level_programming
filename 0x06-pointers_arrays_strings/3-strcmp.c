46646d22e461bbd8b8482a9598bf0afb39e66755
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-29 12:30:20 -0700

    File 3 copying of strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] < s2[i])
-				return (s1[i] - s2[i]);
-			else if (s1[i] > s2[i])
-				return (s1[i] - s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		else
 			return (0);
