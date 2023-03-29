a890320d10c250e35316dd1f745a7b0a32bdfc88
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-03-29 12:22:58 -0700

    File 3 copying of strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: always 0 (success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
