e7ac7dc00eab3fa29ba9a5cd2e5b78a32861055e
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-04-11 04:50:44 -0700

    File 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,21 +3,18 @@
 /**
  * create_array - creates an array of chars
  * @size: size of array
- * @c: char to assign
- * Descriptin: create an array of size size and assign char c
+ * @c: char to be checked
+ *
  * Return: pointer to the array or NULL if it fails
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-	unsigned int i;
+	char *s = malloc(size);
 
-	str = malloc(sizeof(char) * size);
-	if (str == 0 || str == NULL)
+	if (size == 0 || s == 0)
 		return (NULL);
-
-	for (i = 0; i < size ; i++)
-		str[i] = c;
-	return (str);
+	while (size--)
+		s[size] = c;
+	return (s);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
