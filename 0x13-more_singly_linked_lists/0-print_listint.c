2d05157f4b4e82235ac4a26aa3b3232bd2a09861
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-05-02 09:43:06 -0700

    File 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t value = 0;
+	const listint_t *pointer = h;
 
-	while (h != NULL)
+	while (pointer != NULL)
 	{
-		printf("%d\n", h->n);
-		value++;
-		h = h->next;
+		printf("%d\n", pointer->n);
+		value += 1;
+		pointer = pointer->next;
 	}
 	return (value);
 }
