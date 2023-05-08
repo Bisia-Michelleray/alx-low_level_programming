7cd3191af4389b68c2be5ed81d37ae27518b455b
Bisia-Michelleray <bisiamitch7@gmail.com>
2023-05-08 14:52:41 -0700

    git commit -m "untracked files"

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_times_table - prints times tables
- *
- * @n: takes number input
+ * print_times_table - prints timetables
+ * @n: parameter to be checked
+ * Return: always 0 (success)
  */
 
 void print_times_table(int n)
 {
-	int prod, mult, num;
+	int num, mult, prod;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 	{
 		for (num = 0 ; num <= n ; num++)
 		{
@@ -19,20 +19,20 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-
 				prod = num * mult;
-
-				if (prod <= 9)
-					_putchar(' ');
 				if (prod <= 99)
 					_putchar(' ');
-
+				if (prod <= 0)
+					_putchar(' ');
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
-				} else if (prod <= 99 && prod >= 10)
-					_putchar((prod % 10) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
 				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
