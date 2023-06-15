d7e503cf0972de5d4ad0e31e9105d25a05b86fde
Bisia Michelleray <bisiamitch7@gmail.com>
2023-06-15 04:06:05 -0700

    commencing the project

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,7 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+int _strlen(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
