ed137f8a47cbc676789d06058e2b997bb0547830
Bisia Michelleray <bisiamitch7@gmail.com>
2023-06-30 13:29:19 -0700

    writing a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+msg:
+	db "Hello, Holberton", 10,0
+section .text
+	global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17
+	syscall
+	;return (0)
+	mov rax, 60
+	syscall
