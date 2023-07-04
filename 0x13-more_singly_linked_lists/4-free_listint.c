519d37b1066ea8ebcf1a8e0d58b806efc0fac756
Bisia Michelleray <bisiamitch7@gmail.com>
2023-07-04 08:56:35 -0700

    freeing a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: first node of the list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	if (head != NULL)
+	{
+		tmp = head;
+		head = tmp[0].next;
+		free(tmp);
+	}
+	free(head);
+}
